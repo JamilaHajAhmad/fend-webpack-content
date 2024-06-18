b50c9317b254c17e274b2dcb70b07336fb1b53c3
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2024-06-18 19:23:11 +0300

    Plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const json = require('./package.json');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -15,5 +16,12 @@ module.exports = {
                 loader: "json-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+
+    ]
 }
